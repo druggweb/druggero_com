5f3fddf2b4355acdeaf7be6fa9dadf7c8769f468
Nick Ruggero <druggero7@gmail.com>
2018-10-28 02:36:32 -0400

    Added paddle game and car photos

---
+++ breakout_prod/precache-manifest.a47b986c2bd4ea2538f281381988ca01.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e98d46e1641e945f060224521100f984",
+    "url": "./assets/images/paddle.png"
+  },
+  {
+    "revision": "00d7e068523a9fe9c511",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "e6f139b855bb974223bb",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "1524207cbb26f0f0f1a44af5bf15bb6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8ba06c323a8fc17c0044df6df948a1f",
+    "url": "./assets/images/phaser-es6-webpack.jpg"
+  },
+  {
+    "revision": "dfb749fb41edc5c53626b50ec9d48776",
+    "url": "./assets/images/loader-bg.png"
+  },
+  {
+    "revision": "78ea0555ef7648bf378481a5d778d344",
+    "url": "./assets/images/loader-bar.png"
+  },
+  {
+    "revision": "5cad248d276bec86ea099b01bf7a55bd",
+    "url": "./assets/images/brick.png"
+  },
+  {
+    "revision": "50b23fe4d3ed57c121ddf1ecf130d67e",
+    "url": "./assets/images/ball.png"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "./assets/images/.DS_Store"
+  },
+  {
+    "revision": "3585bc041684e8845b639088608b3bb2",
+    "url": "././manifest.json"
+  }
+];
