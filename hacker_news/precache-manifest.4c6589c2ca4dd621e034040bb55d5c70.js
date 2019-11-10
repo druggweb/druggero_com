13ffaeca018055a52c4dfc9c73fb4cd97eda2f98
Nick Ruggero <druggero7@gmail.com>
2019-11-09 21:48:09 -0500

    Added Hacker News

---
+++ hacker_news/precache-manifest.4c6589c2ca4dd621e034040bb55d5c70.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e809f35eb6075ef33b0049e8510d55c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5e6486b0bb20341bb90",
+    "url": "/static/js/2.28436ef2.chunk.js"
+  },
+  {
+    "revision": "54c9f97325ba0a4433fe",
+    "url": "/static/js/main.3dc4483f.chunk.js"
+  },
+  {
+    "revision": "76e6e1b0a4796fdf306b",
+    "url": "/static/js/runtime-main.6421f4ba.js"
+  }
+]);
